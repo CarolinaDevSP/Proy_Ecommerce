12f662f1cb4e94ad2d33bffb09c638fee5341ebc
CarolinaDevSP <Betsy.SP.20@gmail.com>
2022-11-15 20:23:30 -0500

    inicia el manejo de variables de entorno

---
+++ env.js
@@ -0,0 +1,2 @@
+//creo las mismas variables den entorno env
+export const API_URL = import.meta.env.VITE_API_URL
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
+import { API_URL } from "../env"
 
 function App() {
 
   return (
     <div>
-      <h1>HOLA MUNDO CRUEL</h1>
+      <h1>HOLA MUNDO CRUEL {API_URL}</h1>
     </div>
   )
 }
